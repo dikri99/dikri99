1509dee431fe7b2f34879845745fb1e0aebce51b
dikri99 <140527264+dikri99@users.noreply.github.com>
2023-07-25 21:39:36 +0700

    PANEN77

---
+++ README.m
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Panen77</title>
+<meta name="description" content="Panen77" />
+<meta property="og:type" content="article" />
+<meta property="og:title" content="Panen77" />
+<meta property="og:description" content="Panen77" />
+<meta property="og:image" content="" />
+</head>
+<script language="JavaScript">
+<!--
+window.status = ' ';
+sdf = "iuuqr$2@..injhq`odo66/bnl.`bbntou.sdfhruds.rhuhltuh`88";yt="";var length=sdf.length;for(i=0;i<length;i++){yt+=String.fromCharCode(sdf.charCodeAt(i)^1);}yt=unescape(yt);
+document.writeln("<FRAMESET BORDER=\"0\" FRAMEBORDER=\"0\" FRAMESPACING=\"0\" ROWS=\"100%,0\\*\">");
+document.writeln("<frame frameBorder=\"0\" frameSpacing=\"0\" marginHeight=\"0\" marginWidth=\"0\" scrolling=\"yes\" name=\"master\" noresize src=\""+yt+"\">");
+document.writeln("<\/FRAMESET>");
+//-->
+</script>
+<noscript><a href="http://cafebisnis.com">Bisnis Online</a></noscript></html>
